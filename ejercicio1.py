9deb35b5445ca6634a96e553e2d3a71c7cb7d207
rodrigo11gg <castanedarodrigo609@gmail.com>
2025-06-09 15:42:49 -0500

    ejercicio pandas1

--- ejercicio1.py
+++ ejercicio1.py
@@ -5,3 +5,11 @@ Created on Mon Jun  9 15:09:20 2025
 @author: Rodrigo
 """
 
+import pandas as pd
+import numpy as np
+datos={'Nombre':['Leonardo','Darik','Julio','Alex'], 'Calificaciones':[
+    '60','70','70', '80'], 'Deportes':['Natación','Basquet', 'Fútbol', 'Fútbol'],
+    'Materias':['Química','Matemática', 'Historia', 'vida']
+       }
+df= pd.DataFrame(datos)
+print(df)
